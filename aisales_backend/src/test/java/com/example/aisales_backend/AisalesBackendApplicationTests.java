fb355fab0b26202d5998964889027fcfad98a685
Riviya <rivindu.perera3@gmail.com>
2026-05-01 16:04:56 +0530

    Fixed errors in Cucumber Tests

--- aisales_backend/src/test/java/com/example/aisales_backend/AisalesBackendApplicationTests.java
+++ aisales_backend/src/test/java/com/example/aisales_backend/AisalesBackendApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.aisales_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")  // Add this line
 class AisalesBackendApplicationTests {
-
     @Test
     void contextLoads() {
     }
-
 }
